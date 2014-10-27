7e48ed30373fcb91ad044f73705ecc954a714b1d
orchid-hybrid <orchid-hybrid@users.noreply.github.com>
2014-10-27 16:53:44 +0000

    Create print_hex.asm

---
+++ 64/print_hex.asm
@@ -0,0 +1,39 @@
+section .bss
+        hex_buffer resb 16+2
+        hex_buffer_end resb 1
+
+section .text
+global _start
+_start:
+        mov rax,0x6549871456122343
+        call print_hex
+        
+        xor rdi,rdi
+        mov rax,60
+        syscall
+
+print_hex:
+        mov rsi,hex_buffer_end
+        mov rdx,0
+.loop:
+        dec rsi
+        inc rdx
+        test rax,rax
+        jz .done
+        mov bl,0xF
+        and byte bl,al
+        add bl,'0'
+        mov byte [rsi],bl
+        shr rax,4
+        jmp .loop
+.done:
+        dec rsi
+        inc rdx
+        mov byte [rsi],'x'
+        dec rsi
+        inc rdx
+        mov byte [rsi],'0'
+        mov rax,1
+        mov rdi,1
+        syscall
+        ret
